1b599effbbc52c43da27b1d32411e11762ee25bb
Hariharan Subramanian <hariharan_subramanian@homedepot.com>
2021-07-23 20:35:37 -0400

    BST recursive add, lookup

--- src/com/mytrial/BinarySearchTree.java
+++ src/com/mytrial/BinarySearchTree.java
@@ -1,7 +1,5 @@
 package com.mytrial;
 
-import java.net.BindException;
-
 public class BinarySearchTree {
     Node root;
     static class Node {
@@ -16,57 +14,99 @@ public class BinarySearchTree {
         }
     }
 
-    boolean insert(int data) {
-        Node newNode = new Node(data);
-        if (root == null) {
-            root = newNode;
-            return true;
-        } 
-        Node alreadyPresent = lookUp(data);
-        if (alreadyPresent != null) {
-            System.out.println("data already present"+alreadyPresent.data);
-            return true;
-        }
+    // Node insert(Node rootNode, int data) {
+    //     Node newNode = new Node(data);
+    //     if (rootNode == null) {
+    //         this.root = newNode;
+    //         return this.root;
+    //     } 
+    //     // Node alreadyPresent = lookUp(root, data);
+    //     // if (alreadyPresent != null) {
+    //     //     System.out.println("data already present"+alreadyPresent.data);
+    //     //     return true;
+    //     // }
 
-        Node temp = root;
-        while (newNode.data < temp.data && temp.left != null) {
-            temp = temp.left;
-        } 
+    //     // Node temp = root;
+    //     // while (newNode.data < temp.data && temp.left != null) {
+    //     //     temp = temp.left;
+    //     // } 
 
-        while (newNode.data > temp.data && temp.right != null) {
-            temp = temp.right;
-        }
+    //     // while (newNode.data > temp.data && temp.right != null) {
+    //     //     temp = temp.right;
+    //     // }
 
-        if (temp != null) {
-            if (newNode.data < temp.data) {
-                temp.left = newNode;
-            } else {
-                temp.right = newNode;
-            }
-        }
+    //     // if (temp != null) {
+    //     //     if (newNode.data < temp.data) {
+    //     //         temp.left = newNode;
+    //     //     } else {
+    //     //         temp.right = newNode;
+    //     //     }
+    //     // }
+
+    //     if (newNode.data < rootNode.data) {
+    //         this.root.left = insert(rootNode.left, data);
+    //     }
+    //     if (newNode.data > rootNode.data) {
+    //         this.root.right = insert(rootNode.right, data);
+    //     }
         
-        return true;
+    //     return this.root;
+    // }
+
+    void insert(int data) {
+        root = insertRec(root, data);
     }
 
-    Node lookUp(int data) {
+    Node insertRec(Node root, int key)
+    {
+ 
+        /* If the tree is empty,
+           return a new node */
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+ 
+        /* Otherwise, recur down the tree */
+        if (key < root.data)
+            root.left = insertRec(root.left, key);
+        else if (key > root.data)
+            root.right = insertRec(root.right, key);
+ 
+        /* return the (unchanged) node pointer */
+        return root;
+    }
+
+    Node lookUp(Node root, int data) {
         if (root == null) throw new NullPointerException();
 
         if (root.data == data) {
             System.out.println("found data at root -->"+root.data);
             return root;
         }
-        Node temp = root;
-        while(data > temp.data && data != temp.data) {
-            temp = temp.right;
-        }
+        if (data < root.data) {
+            return lookUp(root.left, data);
+        } 
+        return lookUp(root.right, data);
+        // Node temp = root;
+        //     while(temp != null && data > temp.data && data != temp.data) {
+        //         temp = null != temp.right ? temp.right : null;
+        //     }
+    
+        //     while(temp != null && data < temp.data && data != temp.data) {
+        //         temp = null != temp.left ? temp.left : null;
+        //     }
+        //     if (temp != null && temp.data == data) {
+        //         System.out.println("found data after loop -->"+temp.data);
+        //         return temp;
+        //     } else {
+        //         return null;
+        //     }
+    }
 
-        while(data < temp.data && data != temp.data) {
-            temp = temp.left;
-        }
-        
-        System.out.println("found data after loop -->"+temp.data);
+    void remove(int data) {
 
-        return temp;
     }
 
     public static void main(String[] args) {
@@ -80,7 +120,7 @@ public class BinarySearchTree {
         binarySearchTree.insert(15);
         binarySearchTree.insert(170);
 
-        binarySearchTree.lookUp(1);
+        binarySearchTree.lookUp(binarySearchTree.root,170);
 
 
     }
